3859398177aae0dc6122c6afbb7b39ac88233a4e
Mostafa Saeed <mostafa.saeed543@gmail.com>
2016-08-12 21:41:04 +0200

    Removing jQuery & Adding Target

--- src/linker.js
+++ src/linker.js
@@ -1,5 +1,1 @@
-$('head').append('<style type="text/css">*[link]:hover{cursor: pointer;}</style>');
-$('*[link]').click(function(){
-	window.location = $(this).attr('link');
-	return false;
-});
+document.getElementsByTagName('head')[0].innerHTML+='<style type="text/css">*[link]:hover{cursor: pointer;}</style>';for(var i=0;i<document.body.getElementsByTagName("*").length;i++){document.body.getElementsByTagName("*")[i].addEventListener("click",function(){window.open(this.getAttribute('link'),(this.getAttribute('target')||typeof(this.getAttribute('target')))=='undefined'?'_self':this.getAttribute('target'))})}
