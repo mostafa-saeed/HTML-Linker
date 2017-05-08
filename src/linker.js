b6d3db8dd8685c6e650bbbda58a49feff86086c2
Mostafa Saeed <mostafa.saeed543@gmail.com>
2017-05-08 11:15:04 +0200

    docReady

--- src/linker.js
+++ src/linker.js
@@ -1,1 +1,97 @@
-document.getElementsByTagName('head')[0].innerHTML+='<style type="text/css">*[link]:hover{cursor: pointer;}</style>';for(var i=0;i<document.body.getElementsByTagName("*").length;i++){document.body.getElementsByTagName("*")[i].addEventListener("click",function(){window.open(this.getAttribute('link'),this.getAttribute('target')==null?'_self':this.getAttribute('target'));});}
+/* add css selector */
+var css = '*[link]:hover{cursor: pointer;}',
+	head = document.head || document.getElementsByTagName('head')[0],
+	style = document.createElement('style');
+
+style.type = 'text/css';
+if (style.styleSheet){
+	style.styleSheet.cssText = css;
+} else {
+	style.appendChild(document.createTextNode(css));
+}
+head.appendChild(style);
+
+/* Document ready function */
+//docs here: https://github.com/jfriend00/docReady
+
+(function(funcName, baseObj) {
+	// The public function name defaults to window.docReady
+	// but you can pass in your own object and own function name and those will be used
+	// if you want to put them in a different namespace
+	funcName = funcName || "docReady";
+	baseObj = baseObj || window;
+	var readyList = [];
+	var readyFired = false;
+	var readyEventHandlersInstalled = false;
+
+	// call this when the document is ready
+	// this function protects itself against being called more than once
+	function ready() {
+		if (!readyFired) {
+			// this must be set to true before we start calling callbacks
+			readyFired = true;
+			for (var i = 0; i < readyList.length; i++) {
+				// if a callback here happens to add new ready handlers,
+				// the docReady() function will see that it already fired
+				// and will schedule the callback to run right after
+				// this event loop finishes so all handlers will still execute
+				// in order and no new ones will be added to the readyList
+				// while we are processing the list
+				readyList[i].fn.call(window, readyList[i].ctx);
+			}
+			// allow any closures held by these functions to free
+			readyList = [];
+		}
+	}
+
+	function readyStateChange() {
+		if ( document.readyState === "complete" ) {
+			ready();
+		}
+	}
+
+	// This is the one public interface
+	// docReady(fn, context);
+	// the context argument is optional - if present, it will be passed
+	// as an argument to the callback
+	baseObj[funcName] = function(callback, context) {
+		if (typeof callback !== "function") {
+			throw new TypeError("callback for docReady(fn) must be a function");
+		}
+		// if ready has already fired, then just schedule the callback
+		// to fire asynchronously, but right away
+		if (readyFired) {
+			setTimeout(function() {callback(context);}, 1);
+			return;
+		} else {
+			// add the function and context to the list
+			readyList.push({fn: callback, ctx: context});
+		}
+		// if document already ready to go, schedule the ready function to run
+		if (document.readyState === "complete") {
+			setTimeout(ready, 1);
+		} else if (!readyEventHandlersInstalled) {
+			// otherwise if we don't have event handlers installed, install them
+			if (document.addEventListener) {
+				// first choice is DOMContentLoaded event
+				document.addEventListener("DOMContentLoaded", ready, false);
+				// backup is window load event
+				window.addEventListener("load", ready, false);
+			} else {
+				// must be IE
+				document.attachEvent("onreadystatechange", readyStateChange);
+				window.attachEvent("onload", ready);
+			}
+			readyEventHandlersInstalled = true;
+		}
+	}
+})("docReady", window);
+
+/* get all link elements from document body and add click event */
+docReady(function() {
+	document.body.querySelectorAll('*[link]').forEach(function(element, key) {
+		element.addEventListener("click" ,function() {
+			window.open(this.getAttribute('link'),this.getAttribute('target') === null ? '_self' : this.getAttribute('target'));
+		});
+	});	
+});
